0bcdaea68049ab15607c6fa696177819ee4c80bc
Matheus Verissimo <mverissimoo@hotmail.com>
2021-03-07 15:42:36 -0300

    feat(sb): add config

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,15 +1,11 @@
-import React from "react";
-import { Button } from "./Button";
+import React from 'react';
+import { Button } from './Button';
 
 export default {
-  title: "Example/Button",
+  title: 'Example/Button',
   component: Button,
   parameters: {
-    myAddonParameter: `
-<MyComponent boolProp scalarProp={1} complexProp={{ foo: 1, bar: '2' }}>
-  <SomeOtherComponent funcProp={(a) => a.id} />
-</MyComponent>
-`,
+    status: 'stable',
   },
 };
 
@@ -18,22 +14,22 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: "Button",
+  label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: "Button",
+  label: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: "large",
-  label: "Button",
+  size: 'large',
+  label: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: "small",
-  label: "Button",
+  size: 'small',
+  label: 'Button',
 };
